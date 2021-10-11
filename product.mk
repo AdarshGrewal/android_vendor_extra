88b934cad4faec680d717c2a3eebb045ddaea7a1
AdarshGrewal <adarshgrewal@gmail.com>
2021-10-11 20:50:53 +0530

    extra: make google-sans the Default headlines font

--- product.mk
+++ product.mk
@@ -16,3 +16,7 @@ PRODUCT_COPY_FILES += \
     vendor/extra/prebuilt/common/etc/fonts_customization.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml \
     vendor/extra/prebuilt/common/fonts/GoogleSans-Italic.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-Italic.ttf \
     vendor/extra/prebuilt/common/fonts/GoogleSans-Regular.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-Regular.ttf
+
+# Overlay
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/extra/overlay
+DEVICE_PACKAGE_OVERLAYS += vendor/extra/overlay/common
