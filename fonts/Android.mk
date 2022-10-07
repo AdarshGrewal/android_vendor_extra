f1d5afcb8be3f7c4343b70436a233acafe7aabc4
Arian <arian.kulmer@web.de>
2022-10-07 15:40:38 +0530

    extra: Append google-sans font to lineage fonts on build time

---
+++ fonts/Android.mk
@@ -0,0 +1,17 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+fonts_customization := $(TARGET_OUT_PRODUCT)/etc/fonts_customization.xml
+fonts_customization_dummy := $(TARGET_OUT_PRODUCT)/etc/fonts_customization.dummy.xml
+
+$(fonts_customization_dummy): $(fonts_customization)
+	if ! grep -Fq "google-sans" $(fonts_customization); then \
+		sed -i 's|</fonts-modification>||' $(fonts_customization); \
+		cat vendor/extra/fonts/font.google-sans.xml >> $(fonts_customization); \
+		echo "</fonts-modification>" >> $(fonts_customization); \
+	fi
+
+ALL_DEFAULT_INSTALLED_MODULES += $(fonts_customization_dummy)
--- product.mk
+++ product.mk
@@ -17,7 +17,6 @@ endif
 
 # Fonts
 PRODUCT_COPY_FILES += \
-    vendor/extra/prebuilt/common/etc/fonts_customization.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml \
     vendor/extra/prebuilt/common/fonts/GoogleSans-Italic.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-Italic.ttf \
     vendor/extra/prebuilt/common/fonts/GoogleSans-Regular.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-Regular.ttf
 
