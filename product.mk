eec72e875c44aaddd0f4bfa311ea2401864cd41f
AdarshGrewal <adarshgrewal@gmail.com>
2022-10-21 21:11:36 +0530

    extra: remove support for ih8sn

--- product.mk
+++ product.mk
@@ -28,12 +28,3 @@ DEVICE_PACKAGE_OVERLAYS += vendor/extra/overlay/common
 PRODUCT_PACKAGES += \
     ExtraLineageSDKOverlay \
     PixelLauncherIconsOverlay
-
-# I hate Safety Net
-ifneq ("$(wildcard  vendor/extra/prebuilt/etc/ih8sn_$(subst lineage_,,$(TARGET_PRODUCT)).conf)","")
-PRODUCT_PACKAGES += \
-    ih8sn
-
-PRODUCT_COPY_FILES += \
-    vendor/extra/prebuilt/etc/ih8sn_$(subst lineage_,,$(TARGET_PRODUCT)).conf:/system/etc/ih8sn.conf
-endif
