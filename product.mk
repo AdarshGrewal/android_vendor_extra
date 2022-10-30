ffd4172ef81976f8820a0217dca463bbc92d8d3d
AdarshGrewal <adarshgrewal@gmail.com>
2022-10-30 18:44:12 +0530

    extra: Sort everything

--- product.mk
+++ product.mk
@@ -1,12 +1,10 @@
 # Audio
 TARGET_EXCLUDES_AUDIOFX := true
 
-# Gboard side padding
-PRODUCT_PRODUCT_PROPERTIES += \
-    ro.com.google.ime.kb_pad_port_l=4 \
-    ro.com.google.ime.kb_pad_port_r=4 \
-    ro.com.google.ime.kb_pad_land_l=64 \
-    ro.com.google.ime.kb_pad_land_r=64
+# Fonts
+PRODUCT_COPY_FILES += \
+    vendor/extra/prebuilt/common/fonts/GoogleSans-Italic.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-Italic.ttf \
+    vendor/extra/prebuilt/common/fonts/GoogleSans-Regular.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-Regular.ttf
 
 # Google Apps
 ifneq ($(BUILD_VANILLA), true)
@@ -16,18 +14,17 @@ $(call inherit-product, vendor/gms/products/gms.mk)
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.boot.vendor.overlay.theme=com.android.internal.systemui.navbar.gestural;com.google.android.systemui.gxoverlay
 
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.com.google.ime.kb_pad_port_l=4 \
+    ro.com.google.ime.kb_pad_port_r=4 \
+    ro.com.google.ime.kb_pad_land_l=64 \
+    ro.com.google.ime.kb_pad_land_r=64
 endif
 
-# Fonts
-PRODUCT_COPY_FILES += \
-    vendor/extra/prebuilt/common/fonts/GoogleSans-Italic.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-Italic.ttf \
-    vendor/extra/prebuilt/common/fonts/GoogleSans-Regular.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-Regular.ttf
-
 # Overlay
-PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/extra/overlay
-DEVICE_PACKAGE_OVERLAYS += vendor/extra/overlay/common
-
-# Overlay - RRO
 PRODUCT_PACKAGES += \
     ExtraLineageSDKOverlay \
     PixelLauncherIconsOverlay
+
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/extra/overlay
+DEVICE_PACKAGE_OVERLAYS += vendor/extra/overlay/common
