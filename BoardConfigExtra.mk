ece23274c6eefbc69a582a2c72bde2f96828cf54
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-12-08 06:03:09 +0530

    sepolicy: Allow gmscore_app to set system_prop
    Required to set spoofing properties for native PIHooks.
    
    Change-Id: If3ecf44cd471ed5cd4f26b263a27d0d208167cb5

--- BoardConfigExtra.mk
+++ BoardConfigExtra.mk
@@ -3,5 +3,9 @@ ifneq ($(BUILD_VANILLA), true)
 include vendor/pixel/products/board.mk
 endif
 
+# Sepolicy
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
+    vendor/extra/sepolicy/private
+
 # MiuiCamera
 -include vendor/xiaomi/$(PRODUCT_DEVICE)-miuicamera/products/board.mk
