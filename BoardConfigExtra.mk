eb9d0038edd9474f61d7f638edba1ff879e07bc2
AdarshGrewal <adarshgrewal@gmail.com>
2024-04-30 11:57:33 +0530

    extra: Switch to partner_gms and vendor/pixel

--- BoardConfigExtra.mk
+++ BoardConfigExtra.mk
@@ -1,7 +1,6 @@
 # Google Apps
 ifneq ($(BUILD_VANILLA), true)
-include vendor/gms/products/board.mk
-include vendor/gms-pixel/products/board.mk
+include vendor/pixel/products/board.mk
 
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
 	vendor/extra/sepolicy/common/dynamic
