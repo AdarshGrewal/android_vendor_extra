bb2cebc836043ffa62d3c5f33a6b27ca418b8c25
AdarshGrewal <adarshgrewal@gmail.com>
2024-09-09 22:23:53 +0530

    extra: Build ParanoidSense

--- product.mk
+++ product.mk
@@ -25,6 +25,20 @@ DEVICE_PACKAGE_OVERLAYS += vendor/extra/overlay/common
 
 endif
 
+# Face Unlock
+TARGET_FACE_UNLOCK_SUPPORTED ?= true
+
+ifeq ($(TARGET_FACE_UNLOCK_SUPPORTED),true)
+PRODUCT_PACKAGES += \
+    ParanoidSense
+
+PRODUCT_SYSTEM_EXT_PROPERTIES += \
+    ro.face.sense_service=true
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
+endif
+
 PRODUCT_PACKAGES += \
     ExtraPIFrameworksResTarget
 
