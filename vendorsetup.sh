e0794e6d4b62383315596bfb106e93730ec1a2f3
AdarshGrewal <adarshgrewal@gmail.com>
2024-12-07 17:13:10 +0530

    extra: Use thin lto by default

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -27,6 +27,14 @@ enable_gms() {
     fi
 }
 
+enable_thin_lto() {
+    local enable_thin_lto=${1:-true}
+    if [[ "${enable_thin_lto}" == "true" ]]; then
+        export KERNEL_LTO=thin
+        echo -e "\e[32m[INFO]\e[0m Forcing thin lto."
+    fi
+}
+
 release() {
     device=${1}
     project="$(basename ${ANDROID_BUILD_TOP})"
@@ -197,3 +205,4 @@ release() {
 
 enable_ccache
 enable_gms
+enable_thin_lto
