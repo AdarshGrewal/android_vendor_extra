41ba8536d4e6ea3e7bea544aaacb1e15e7ef8c59
AdarshGrewal <adarshgrewal@gmail.com>
2026-09-09 09:27:41 +0530

    extra: Drop pixel_2016_exclusive sysconfig
    Change-Id: Ia1ebf38611b7ab66e0bf81e51b7d0dc52fb95571

--- product.mk
+++ product.mk
@@ -31,7 +31,3 @@ PRODUCT_PACKAGES += \
 # Spoofify
 PRODUCT_PACKAGES += \
     Spoofify
-
-# Sysconfig
-PRODUCT_COPY_FILES += \
-    vendor/extra/prebuilt/common/etc/sysconfig/pixel_2016_exclusive.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/pixel_2016_exclusive.xml
